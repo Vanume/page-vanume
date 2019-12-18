d5cf03fde2ec4bec4fad2371d116382e5f562695
Diseño <MKT@MacBook-Pro-de-Luis.local>
2019-12-18 13:38:20 -0600

    Se inició componente Earth

---
+++ src/components/earth/earth.component.js
@@ -0,0 +1,31 @@
+import React, { useRef, useMemo } from 'react';
+import { useFrame, useLoader } from 'react-three-fiber';
+import { TextureLoader } from 'three';
+
+import url3 from '../../static/images/earthmap1k.jpg';
+import url2 from '../../static/images/earthspec1k.jpg';
+import url1 from '../../static/images/earthbump1k.jpg';
+
+import './earth.styles.scss';
+
+const Earth = () => {
+    const ref = useRef();
+    // const texture = useMemo(() => new THREE.ImageUtils.loadTexture(url3));
+    // const bump = useMemo(() => new THREE.ImageUtils.loadTexture(url1));
+    const [bumpMap, specMap, normalMap] = useLoader(TextureLoader, [url1, url2, url3]);
+    useFrame(() => (ref.current.rotation.y -= 0.001))
+    return (
+        
+      <mesh
+        ref={ref}
+        onClick={e => console.log('click')}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}>
+        <sphereGeometry attach="geometry" args={[2, 64, 64]} />
+        <meshPhongMaterial attach='material' map={normalMap} bumpMap={bumpMap} bumpScale={0.05}  />
+        
+      </mesh>
+    )
+}
+
+export default Earth;
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import { Canvas } from 'react-three-fiber';
+import * as THREE from 'three'
 
 import Background from '../../components/background/background.component';
+import Earth from '../../components/earth/earth.component';
 
 import './home.styles.scss'
 
@@ -9,6 +12,16 @@ const Home = () => {
         <>
         <p>HOME</p>
         <Background/>
+        <Canvas camera={{ position: [0, 0, 10], fov: 40, far: 10000 }} 
+         onCreated={({ gl }) => {
+            gl.gammaInput = true
+            gl.toneMapping = THREE.ACESFilmicToneMapping
+          }} >
+        <Suspense fallback={null}>
+            <Earth />
+        </Suspense> 
+                <pointLight intensity={3} position={[10, 10, 10]} />
+        </Canvas>
         </>
     )
 }
